da275c8e404674e981b4a5b72f772842599e3364
David Kanenwisher <d.kanen@gmail.com>
2021-07-05 16:15:26 -0500

    Start the project

---
+++ Metal001DrawLoop/Metal001DrawLoop/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Metal001DrawLoop
+//
+//  Created by David Kanenwisher on 7/5/21.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Metal001DrawLoop/Metal001DrawLoop/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Metal001DrawLoop
+//
+//  Created by David Kanenwisher on 7/5/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
