2d8366509d2aa7cc5be39658b31ec348b5a00da8
David Kanenwisher <d.kanen@gmail.com>
2021-07-06 09:19:33 -0500

    Add some comments

--- Metal001DrawLoop/Metal001DrawLoop/AppDelegate.swift
+++ Metal001DrawLoop/Metal001DrawLoop/AppDelegate.swift
@@ -13,17 +13,29 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         print(#function)
+        print("""
+              Let's get things started!🎊
+              """)
+
+
         // I think this is the best way to get a hold of the first window shown.
         // https://stackoverflow.com/questions/40008141/nsapplicationdelegate-not-working-without-storyboard/41029060
         let viewController = NSApplication.shared.windows.first!.contentViewController as! ViewController
+
+        // Pass the Drawer.Builder into the view controller. This is to allow the Drawer to be initialized with all of
+        // the bits it needs to run that could be shared across many "Drawers": MTLDevice, MTLLibrary, MTLPipelines,
+        // models, etc. It's also helpful to delay attaching to the MTLView as the view delegate. The moment that
+        // happens drawing starts and everything needs to be ready for that.
         viewController.setup(drawerBuilder: Drawer.Builder())
         viewController.startDrawing()
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
+        print(#function)
+        print("""
+              Time to close up the shop and head home...
+              """)
     }
-
-
 }
 
--- Metal001DrawLoop/Metal001DrawLoop/Drawer.swift
+++ Metal001DrawLoop/Metal001DrawLoop/Drawer.swift
@@ -11,10 +11,16 @@ class Drawer: NSObject {
     init(view: MTKView) {
         self.view = view
 
+        // Initialize NSObject so it can do what it needs to do.
         super.init()
 
+        // Set the Drawer to the view delegate. Make sure the Drawer is fully initialized before doing this since
+        // the object has now escaped. The fact that the objects escapes at this point makes me wonder if this isn't
+        // the best place to do this. It seems a bit sneaky if you're trying to understand the threading model if
+        // the an object escapes during the initializer.
         view.delegate = self
 
+        // Call mtkView to set the initial size of the viewport. More happens here later.
         mtkView(view, drawableSizeWillChange: view.bounds.size)
     }
 
