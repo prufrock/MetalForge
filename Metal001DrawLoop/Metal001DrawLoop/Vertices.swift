ba9c48c67e0c17eff2eb93ea32d372c5bbae8ceb
David Kanenwisher <d.kanen@gmail.com>
2021-07-06 12:51:33 -0500

    Create vertices struct

---
+++ Metal001DrawLoop/Metal001DrawLoop/Vertices.swift
@@ -0,0 +1,11 @@
+//
+// Created by David Kanenwisher on 7/6/21.
+//
+
+// I'm going to start by making this a struct and see how far I get.
+// So far this seems like a pretty thin wrapper. I'm hoping it provides an decent abstraction to hang
+// some helpful functions on. It might be premature to create this class. Sometimes it's nice to have
+// a place where I can ideas on.
+struct Vertices {
+    let vertices: [Point]
+}
