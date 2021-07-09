cc8bb113a0569ee9b3ba3f1d2d21f181341b8304
David Kanenwisher <d.kanen@gmail.com>
2021-07-08 20:20:31 -0500

    Start the next project on perspective

---
+++ Metal002Perspective/Metal002Perspective/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Metal002Perspective
+//
+//  Created by David Kanenwisher on 7/8/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
