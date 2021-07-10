8985f4939f88487e87a1d4828b5c73229a74d4bc
David Kanenwisher <d.kanen@gmail.com>
2021-07-10 18:59:34 -0500

    Work on perspective begins

--- Metal002Perspective/Metal002Perspective/SimdFloat4x4Ext.swift
+++ Metal002Perspective/Metal002Perspective/SimdFloat4x4Ext.swift
@@ -5,12 +5,16 @@
 import simd
 
 extension float4x4 {
-    static func scaleX(_ scalar: Float) -> simd_float4x4 {
+    static func scaleX(_ scalar: Float) -> float4x4 {
         float4x4(
                 [scalar, 0, 0, 0],
                 [0, 1, 0, 0],
                 [0, 0, 1, 0],
                 [0, 0, 0, 1]
         )
+    }
+
+    static func perspectiveProjection() -> float4x4 {
+        matrix_identity_float4x4
     }
 }
