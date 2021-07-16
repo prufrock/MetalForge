d9ba2b0f1fa2191f4245121c37f1f6612eecc54d
David Kanenwisher <d.kanen@gmail.com>
2021-07-15 20:09:21 -0500

    Invert the relationship between the near plane and the camera
    When the near plane is smaller objects should appear nearer and when it's bigger objects should appear further away.

--- Metal002Perspective/Metal002Perspective/SimdFloat4x4Ext.swift
+++ Metal002Perspective/Metal002Perspective/SimdFloat4x4Ext.swift
@@ -18,10 +18,10 @@ extension float4x4 {
     // my transformations in the wrong order. I still have a problem though where increasing the value of the near plane
     // moves the object closer when it should be moved further away. I'm a little bit stumped by this one and wonder if
     // I am not fully understanding how W is involved in the perspective divide.
-    static func perspectiveProjection(nearPlane: Float) -> float4x4 {
+    static func perspectiveProjection(nearPlane: Float, farPlane: Float) -> float4x4 {
         float4x4(
-                [nearPlane, 0, 0, 0],
-                [0, nearPlane, 0, 0],
+                [(farPlane / farPlane - nearPlane), 0, 0, 0],
+                [0, (farPlane / farPlane - nearPlane), 0, 0],
                 [0, 0, 1, 1],
                 [0, 0, 0, 1]
         )
