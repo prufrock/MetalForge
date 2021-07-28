8f6733afb470beba2dc8dfd1ada43bae301941f0
David Kanenwisher <d.kanen@gmail.com>
2021-07-27 21:37:01 -0500

    Start work on the Matrix Calculator
    I dislike doing matrix math and also dislike typing [{1},{2},{3}] or whatever extra characters I have to type to find the dot product of a matrix and a vector.

---
+++ Matrix Calculator/Shared/Matrix_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Matrix_CalculatorApp.swift
+//  Shared
+//
+//  Created by David Kanenwisher on 7/27/21.
+//
+
+import SwiftUI
+
+@main
+struct Matrix_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
