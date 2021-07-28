35b430c8d25bafc6da32f5f3a20e80daeeb30573
David Kanenwisher <d.kanen@gmail.com>
2021-07-27 22:42:32 -0500

    Add the fields for a matrix

--- Matrix Calculator/Shared/ContentView.swift
+++ Matrix Calculator/Shared/ContentView.swift
@@ -8,14 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var vector: [String] = ["0.0", "0.0", "0.0", "0.0"]
+    @State private var vector: [[String]] = [["0.0", "0.0", "0.0", "0.0"]]
+
+    @State private var vectorFunction: [[String]] = [
+        ["1.0", "0.0", "0.0", "0.0"],
+        ["0.0", "1.0", "0.0", "0.0"],
+        ["0.0", "0.0", "1.0", "1.0"],
+        ["0.0", "0.0", "0.0", "1.0"]
+    ]
 
     var body: some View {
-        VStack{
-            MatrixElement(matrix: $vector, index: 0)
-            MatrixElement(matrix: $vector, index: 1)
-            MatrixElement(matrix: $vector, index: 2)
-            MatrixElement(matrix: $vector, index: 3)
+        HStack{
+            VStack{
+                HStack{
+                    MatrixElement(matrix: $vectorFunction, index: [0,0])
+                    MatrixElement(matrix: $vectorFunction, index: [0,1])
+                    MatrixElement(matrix: $vectorFunction, index: [0,2])
+                    MatrixElement(matrix: $vectorFunction, index: [0,3])
+                }
+                HStack {
+                    MatrixElement(matrix: $vectorFunction, index: [1,0])
+                    MatrixElement(matrix: $vectorFunction, index: [1,1])
+                    MatrixElement(matrix: $vectorFunction, index: [1,2])
+                    MatrixElement(matrix: $vectorFunction, index: [1,3])
+                }
+                HStack {
+                    MatrixElement(matrix: $vectorFunction, index: [2,0])
+                    MatrixElement(matrix: $vectorFunction, index: [2,1])
+                    MatrixElement(matrix: $vectorFunction, index: [2,2])
+                    MatrixElement(matrix: $vectorFunction, index: [2,3])
+                }
+                HStack {
+                    MatrixElement(matrix: $vectorFunction, index: [3,0])
+                    MatrixElement(matrix: $vectorFunction, index: [3,1])
+                    MatrixElement(matrix: $vectorFunction, index: [3,2])
+                    MatrixElement(matrix: $vectorFunction, index: [3,3])
+                }
+            }.padding(10)
+            VStack{
+                MatrixElement(matrix: $vector, index: [0])
+                MatrixElement(matrix: $vector, index: [1])
+                MatrixElement(matrix: $vector, index: [2])
+                MatrixElement(matrix: $vector, index: [3])
+            }
         }
     }
 }
--- Matrix Calculator/Shared/SwiftUIViews/MatrixElement.swift
+++ Matrix Calculator/Shared/SwiftUIViews/MatrixElement.swift
@@ -8,17 +8,27 @@
 import SwiftUI
 
 struct MatrixElement: View {
-    @Binding var matrix: [String]
-    var index: Int
+    @Binding var matrix: [[String]]
+    var index: [Int]
 
     var body: some View {
-        TextField("", text: $matrix[index])
+        TextField("", text: element())
             .frame(width: 30, height: 30, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+    }
+
+    func element() -> Binding<String> {
+        if index.count == 1 {
+            return $matrix[0][index[0]]
+        } else if index.count == 2 {
+            return $matrix[index[0]][index[1]]
+        } else {
+            fatalError("What!? This wasn't supposed to be an index size: " + String(index.count))
+        }
     }
 }
 
 struct MatrixElement_Previews: PreviewProvider {
     static var previews: some View {
-        MatrixElement(matrix: .constant(["0.0"]), index: 0)
+        MatrixElement(matrix: .constant([["0.0"]]), index: [0])
     }
 }
