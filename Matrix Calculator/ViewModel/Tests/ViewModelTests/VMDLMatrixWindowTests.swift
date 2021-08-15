e472b3d8860a35a7190d27a33a1605b2a5953702
David Kanenwisher <d.kanen@gmail.com>
2021-08-15 15:31:41 -0500

    Remove duplicate test

--- Matrix Calculator/ViewModel/Tests/ViewModelTests/VMDLMatrixWindowTests.swift
+++ Matrix Calculator/ViewModel/Tests/ViewModelTests/VMDLMatrixWindowTests.swift
@@ -14,19 +14,6 @@ final class VMDLMatrixWindowTests: XCTestCase {
         XCTAssertEqual(windowId, window.id)
     }
 
-    func testCreateApplicationWithUndoButton() {
-        let windowID = UUID(uuidString: "e3e4d9c2-0a86-4ac1-9847-44d37b67681b")!
-        var app = application(id: windowID) {
-            $0.undoButton(
-                VMDLButton(id: UUID(uuidString: "a14fbeec-3c91-4e30-8d25-91b237de41a4")!, disabled: true)
-            ).dotProductButton(
-                VMDLButton(id: UUID(uuidString: "663c9bf7-e004-4de4-8588-283b3f1c3745")!, disabled: true)
-            )
-        }.create()
-
-        XCTAssertEqual(windowID, app.id)
-    }
-
     func testChangeStateOfButton() {
         var app = application(id: UUID(uuidString: "e3e4d9c2-0a86-4ac1-9847-44d37b67681b")!) {
             $0.undoButton(
