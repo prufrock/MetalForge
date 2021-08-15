73e6f8e9c5672e3ee3bba9f166bd05a0f12fb706
David Kanenwisher <d.kanen@gmail.com>
2021-08-14 21:42:29 -0500

    Create VMDLApplication and expose firstWindow just to make things easy

---
+++ Matrix Calculator/ViewModel/Sources/ViewModel/VMDLApplication.swift
@@ -0,0 +1,57 @@
+//
+// Created by David Kanenwisher on 8/14/21.
+//
+
+import Foundation
+
+@available(iOS 13.0, *)
+@available(macOS 10.15, *)
+public class VMDLApplication {
+    public let id: UUID
+    public let firstWindow: VMDLMatrixWindow
+
+    public init(id: UUID, firstWindow: VMDLMatrixWindow) {
+        self.id = id
+        self.firstWindow = firstWindow
+    }
+
+    public struct Builder {
+
+        private var id: UUID
+        private var firstWindow: VMDLMatrixWindow?
+
+        public init(id: UUID) {
+            self.id = id
+        }
+
+        private init(
+                id: UUID,
+                firstWindow: VMDLMatrixWindow
+        ) {
+            self.id = id
+            self.firstWindow = firstWindow
+        }
+
+        public func firstWindow(id: UUID, _ lambda: (VMDLMatrixWindow.Builder) -> VMDLMatrixWindow.Builder) -> Self {
+            let builder = VMDLMatrixWindow.Builder(id: id)
+            return Self(
+                    id: id,
+                    firstWindow: lambda(builder).create()
+            )
+        }
+
+        public func create() -> VMDLApplication {
+            VMDLApplication(
+                    id: id,
+                    firstWindow: firstWindow!
+            )
+        }
+    }
+}
+
+@available(iOS 13.0, *)
+@available(macOS 10.15, *)
+public func vmdlApplication(id: UUID, using lambda: (VMDLApplication.Builder) -> VMDLApplication.Builder) -> VMDLApplication.Builder {
+    let builder = VMDLApplication.Builder(id: id);
+    return lambda(builder)
+}
---
+++ Matrix Calculator/ViewModel/Tests/ViewModelTests/VMDLApplicationTests.swift
@@ -0,0 +1,22 @@
+//
+// Created by David Kanenwisher on 8/1/21.
+//
+
+import XCTest
+@testable import ViewModel
+
+final class VMDLApplicationTests: XCTestCase {
+
+    func testCreateApplication() {
+        let appId = UUID(uuidString: "e3e4d9c2-0a86-4ac1-9847-44d37b67681b")!
+
+        let app = vmdlApplication(id: appId) {
+            $0.firstWindow(id: UUID(uuidString: "e3e4d9c2-0a86-4ac1-9847-44d37b67681b")!) {
+                $0.undoButton(VMDLButton(id: UUID(uuidString: "a14fbeec-3c91-4e30-8d25-91b237de41a4")!, disabled: true))
+                .dotProductButton(VMDLButton(id: UUID(uuidString: "663c9bf7-e004-4de4-8588-283b3f1c3745")!, disabled: true))
+            }
+        }.create()
+
+        XCTAssertEqual(appId, app.id)
+    }
+}
