c622a6f8c0c7d932fabe1bc015f4970fc63d8c2e
David Kanenwisher <d.kanen@gmail.com>
2021-08-14 21:43:14 -0500

    Switch to creating the application in Matrix_CalculatorApp

--- Matrix Calculator/Shared/Matrix_CalculatorApp.swift
+++ Matrix Calculator/Shared/Matrix_CalculatorApp.swift
@@ -10,21 +10,23 @@ import ViewModel
 
 @main
 struct Matrix_CalculatorApp: App {
-    var application: VMDLMatrixWindow
+    var application: VMDLApplication
 
     init() {
-        application = ViewModel.application(id: UUID(uuidString: "4e6ecfae-9e8d-4464-ba48-976e7f8ed413")!) {
-            return $0.undoButton(
-                VMDLButton(id: UUID(uuidString: "a14fbeec-3c91-4e30-8d25-91b237de41a4")!, disabled: true)
-            ).dotProductButton(
-                VMDLButton(id: UUID(uuidString: "a14fbeec-3c91-4e30-8d25-91b237de41a4")!, disabled: false)
-            )
+        application = ViewModel.vmdlApplication(id: UUID(uuidString: "734f9abe-f805-4ba9-93f8-c5025ab546b8")!) {
+            $0.firstWindow(id: UUID(uuidString: "4e6ecfae-9e8d-4464-ba48-976e7f8ed413")!) {
+                $0.undoButton(
+                    VMDLButton(id: UUID(uuidString: "a14fbeec-3c91-4e30-8d25-91b237de41a4")!, disabled: true)
+                ).dotProductButton(
+                    VMDLButton(id: UUID(uuidString: "a14fbeec-3c91-4e30-8d25-91b237de41a4")!, disabled: false)
+                )
+            }
         }.create()
     }
 
     var body: some Scene {
         WindowGroup {
-            ContentView(app: application)
+            ContentView(app: application.firstWindow)
         }
     }
 }
