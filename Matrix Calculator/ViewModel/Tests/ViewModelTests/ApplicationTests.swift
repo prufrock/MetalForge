8d9450a58313b7b94497dea6f8303782597a6028
David Kanenwisher <d.kanen@gmail.com>
2021-08-14 20:40:05 -0500

    Remove unnecessary returns

--- Matrix Calculator/ViewModel/Tests/ViewModelTests/ApplicationTests.swift
+++ Matrix Calculator/ViewModel/Tests/ViewModelTests/ApplicationTests.swift
@@ -10,7 +10,7 @@ final class ApplicationTests: XCTestCase {
     func testCreateApplicationWithUndoButton() {
         let appID = UUID(uuidString: "e3e4d9c2-0a86-4ac1-9847-44d37b67681b")!
         var app = application(id: appID) {
-            return $0.undoButton(
+            $0.undoButton(
                 VMDLButton(id: UUID(uuidString: "a14fbeec-3c91-4e30-8d25-91b237de41a4")!, disabled: true)
             ).dotProductButton(
                 VMDLButton(id: UUID(uuidString: "663c9bf7-e004-4de4-8588-283b3f1c3745")!, disabled: true)
@@ -23,7 +23,7 @@ final class ApplicationTests: XCTestCase {
     func testChangeStateOfButton() {
         let appID = UUID(uuidString: "e3e4d9c2-0a86-4ac1-9847-44d37b67681b")!
         var app = application(id: appID) {
-            return $0.undoButton(
+            $0.undoButton(
                 VMDLButton(id: UUID(uuidString: "a14fbeec-3c91-4e30-8d25-91b237de41a4")!, disabled: true)
             ).dotProductButton(
                 VMDLButton(id: UUID(uuidString: "70415f15-ce34-4cce-8200-8d7647e2ec71")!, disabled: true)
