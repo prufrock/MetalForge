5ff5e7a588429780e77e0e79d179a609eff1b67f
David Kanenwisher <d.kanen@gmail.com>
2021-08-20 13:44:02 -0500

    Start figuring out the model

--- Matrix Calculator/MCLCModel/Sources/MCLCModel/MCLCModel.swift
+++ Matrix Calculator/MCLCModel/Sources/MCLCModel/MCLCModel.swift
@@ -1,3 +1,14 @@
-struct MCLCModel {
+struct MCLCSingleWindowModel: MCLCModel {
     var text = "Hello, World!"
+    let vectorInput: String = ""
+
+    public func snapshot() -> MCLCModel {
+        return self
+    }
+}
+
+protocol MCLCModel {
+    var vectorInput: String { get }
+
+    func snapshot() -> MCLCModel
 }
--- Matrix Calculator/MCLCModel/Tests/MCLCModelTests/MCLCModelTests.swift
+++ Matrix Calculator/MCLCModel/Tests/MCLCModelTests/MCLCModelTests.swift
@@ -6,6 +6,11 @@ final class MCLCModelTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(MCLCModel().text, "Hello, World!")
+        XCTAssertEqual(MCLCSingleWindowModel().text, "Hello, World!")
+    }
+
+    func testInitialize() {
+        let model = MCLCSingleWindowModel()
+        XCTAssertEqual("", model.snapshot().vectorInput)
     }
 }
