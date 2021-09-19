2abcd9037052b9d9ace4cd7c530f84ab90e0365d
David Kanenwisher <d.kanen@gmail.com>
2021-09-19 16:05:38 -0500

    Start Metal003GameLoop

---
+++ Metal003GameLoop/Metal003GameLoop/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Metal003GameLoop
+//
+//  Created by David Kanenwisher on 9/19/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
