930bb2a7fe02b8f7c13b5931fa024f8cc839fd32
David Kanenwisher <d.kanen@gmail.com>
2021-09-19 20:40:40 -0500

    Metal003 Make it so MetalBits sets more stuff up
    I'm going to try making MetalBits a container for all of the pretty much constant Metal stuff needed for each render. I figure this might make it a little easier to setup and know where to find stuff.

---
+++ Metal003GameLoop/Metal003GameLoop/Shared/MetalBits.swift
@@ -0,0 +1,54 @@
+//
+//  MetalBits.swift
+//  Metal003GameLoop
+//
+//  Created by David Kanenwisher on 9/19/21.
+//
+
+import MetalKit
+
+struct MetalBits {
+    let view: MTKView
+    let device: MTLDevice
+    let pipelines: [PipelineName: MTLRenderPipelineState]
+    let commandQueue: MTLCommandQueue
+
+    func create(view: MTKView) -> MetalBits {
+        guard let device = MTLCreateSystemDefaultDevice() else {
+            fatalError("""
+                       I looked in the computer and didn't find a device...sorry =/
+                       """)
+        }
+
+        guard let commandQueue = device.makeCommandQueue() else {
+            fatalError("""
+                       What?! No comand queue. Come on!
+                       """)
+        }
+
+        guard let library = device.makeDefaultLibrary() else {
+            fatalError("""
+                       What in the what?! The library couldn't be loaded.
+                       """)
+        }
+
+        let pipelineDescriptor = MTLRenderPipelineDescriptor()
+        pipelineDescriptor.vertexFunction = library.makeFunction(name: "vertex_main")
+        pipelineDescriptor.fragmentFunction = library.makeFunction(name: "fragment_main")
+        pipelineDescriptor.colorAttachments[0].pixelFormat = .bgra8Unorm
+
+        let defaultPipelineState = try! device.makeRenderPipelineState(descriptor: pipelineDescriptor)
+
+
+        return MetalBits(
+            view: view,
+            device: MTLCreateSystemDefaultDevice()!,
+            pipelines: [.simple: defaultPipelineState],
+            commandQueue: commandQueue
+        )
+    }
+
+    enum PipelineName {
+        case simple
+    }
+}
