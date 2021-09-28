2a454b1e2d0e80643c2c1acff70a344133a9b368
David Kanenwisher <d.kanen@gmail.com>
2021-09-27 20:45:22 -0500

    Metal004 It begins!

---
+++ Metal004RenderNObjects/Metal004RenderNObjects/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Metal004RenderNObjects
+//
+//  Created by David Kanenwisher on 9/27/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
