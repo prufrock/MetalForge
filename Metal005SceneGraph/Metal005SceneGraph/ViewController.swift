896873f1dec2868816708511f09b84bfc979bd09
David Kanenwisher <d.kanen@gmail.com>
2021-10-05 14:25:50 -0500

    Start Metal005 SceneGraph

---
+++ Metal005SceneGraph/Metal005SceneGraph/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Metal005SceneGraph
+//
+//  Created by David Kanenwisher on 10/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
