e584528b8a3a6815a8a58f2ee10aff1116eee282
David Kanenwisher <d.kanen@gmail.com>
2021-11-24 15:27:15 -0600

    Convert using SIMD

--- Metal006Buttons/Metal006ButtonsTests/DisplayToWorldTests.swift
+++ Metal006Buttons/Metal006ButtonsTests/DisplayToWorldTests.swift
@@ -8,16 +8,16 @@ import simd
 class DisplayToWorldTests: XCTestCase {
 
     func testDisplayToWorld() throws {
-        let display: (CGFloat, CGFloat) = (414.0, 896.0)
-        let world: (CGFloat, CGFloat) = (1, -1)
+        let display: SIMD2<Float> = SIMD2<Float>(414.0, 896.0)
+        let world: SIMD2<Float> = SIMD2<Float>(1, -1)
 
         let ratio = displayToWorld(display: display, world: world)
 
-        XCTAssertEqual(world.0, display.0 * ratio.0)
-        XCTAssertEqual(world.1, display.1 * ratio.1)
+        XCTAssertEqual(world[0], display[0] * ratio[0])
+        XCTAssertEqual(world[1], display[1] * ratio[1])
     }
 
-    func displayToWorld(display: (CGFloat, CGFloat), world: (CGFloat, CGFloat)) -> (CGFloat, CGFloat) {
-        (world.0 / display.0, world.1 / display.1)
+    func displayToWorld(display: SIMD2<Float>, world: SIMD2<Float>) -> SIMD2<Float> {
+        SIMD2<Float>(world[0] / display[0], world[1] / display[1])
     }
 }
