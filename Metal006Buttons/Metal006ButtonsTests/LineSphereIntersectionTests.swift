4ac642271d4b141dfb182ad4cf00fb69f4cf3bc1
David Kanenwisher <d.kanen@gmail.com>
2021-11-24 21:07:57 -0600

    Give Ray a home

--- Metal006Buttons/Metal006ButtonsTests/LineSphereIntersectionTests.swift
+++ Metal006Buttons/Metal006ButtonsTests/LineSphereIntersectionTests.swift
@@ -22,24 +22,3 @@ class LineSphereIntersectionTests: XCTestCase {
         XCTAssertFalse(ray.intersects(with: sphere))
     }
 }
-
-struct Ray {
-    let origin: SIMD3<Float>
-    let target: SIMD3<Float>
-    var displacement: SIMD3<Float> {
-        get {
-            target - origin
-        }
-    }
-
-    func intersects(with sphere: Sphere) -> Bool {
-        //discriminant
-        let a = simd_dot(displacement, displacement)
-        let b = 2.0 * simd_dot((origin - sphere.center), displacement)
-        let oc = origin - sphere.center
-        let c = simd_dot(oc, oc) - (sphere.radius * sphere.radius)
-        let discriminant = (b * b) - 4 * a * c
-
-        return discriminant >= 0
-    }
-}
