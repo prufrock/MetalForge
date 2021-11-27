4563a68b6a7aa9685e84d21f2c29673d5f172e1e
David Kanenwisher <d.kanen@gmail.com>
2021-11-26 19:50:47 -0600

    Use type aliases for float2 and float3 in tests.

--- Metal006Buttons/Metal006ButtonsTests/DisplayToWorldTests.swift
+++ Metal006Buttons/Metal006ButtonsTests/DisplayToWorldTests.swift
@@ -10,30 +10,30 @@ import simd
 class DisplayToNdcTests: XCTestCase {
 
     func testDisplayToNdc() throws {
-        let display: SIMD2<Float> = SIMD2<Float>(414.0, 896.0)
-        let world: SIMD2<Float> = SIMD2<Float>(1, -1)
+        let display: float2 = float2(414.0, 896.0)
+        let world: float2 = float2(1, -1)
 
-        var coords: SIMD2<Float> = display.displayToNdc(display: display)
+        var coords: float2 = display.displayToNdc(display: display)
 
         XCTAssertEqual(world[0], coords[0])
         XCTAssertEqual(world[1], coords[1])
 
-        coords = SIMD2<Float>(display.x / 2, display.y / 2).displayToNdc(display: display)
+        coords = float2(display.x / 2, display.y / 2).displayToNdc(display: display)
 
         XCTAssertEqual(0, coords[0])
         XCTAssertEqual(0, coords[1])
     }
 
     func testDisplayToNdc4x4() throws {
-        let display: SIMD2<Float> = SIMD2<Float>(414.0, 896.0)
-        let world: SIMD2<Float> = SIMD2<Float>(1, -1)
+        let display: float2 = float2(414.0, 896.0)
+        let world: float2 = float2(1, -1)
 
         var coords: float4x4 = display.displayToNdc(display: display)
 
         XCTAssertEqual(world[0], coords[0][0])
         XCTAssertEqual(world[1], coords[1][1])
 
-        coords = SIMD2<Float>(display.x / 2, display.y / 2).displayToNdc(display: display)
+        coords = float2(display.x / 2, display.y / 2).displayToNdc(display: display)
 
         XCTAssertEqual(0, coords[0][0])
         XCTAssertEqual(0, coords[1][1])
--- Metal006Buttons/Metal006ButtonsTests/LineSphereIntersectionTests.swift
+++ Metal006Buttons/Metal006ButtonsTests/LineSphereIntersectionTests.swift
@@ -9,15 +9,15 @@ import simd
 class LineSphereIntersectionTests: XCTestCase {
 
     func testIntersects() throws {
-        let sphere = Sphere(center: SIMD3<Float>(5,0,0), radius: 0.1)
-        let ray = Ray(origin: SIMD3<Float>(0,0,0), target: SIMD3<Float>(5.0, 0.0, 0.0))
+        let sphere = Sphere(center: float3(5,0,0), radius: 0.1)
+        let ray = Ray(origin: float3(0,0,0), target: float3(5.0, 0.0, 0.0))
 
         XCTAssertTrue(ray.intersects(with: sphere))
     }
 
     func testDoesntIntersect() throws {
-        let sphere = Sphere(center: SIMD3<Float>(5,0,0), radius: 0.1)
-        let ray = Ray(origin: SIMD3<Float>(0,0,0), target: SIMD3<Float>(5.0, 3.0, 0.0))
+        let sphere = Sphere(center: float3(5,0,0), radius: 0.1)
+        let ray = Ray(origin: float3(0,0,0), target: float3(5.0, 3.0, 0.0))
 
         XCTAssertFalse(ray.intersects(with: sphere))
     }
