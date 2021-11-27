62eabd3a5b866d6d369c59d4d370f56ac3e4515d
David Kanenwisher <d.kanen@gmail.com>
2021-11-26 19:22:15 -0600

    Give float2 extensions a home

--- Metal006Buttons/Metal006Buttons/Shared/Game/SceneGraph/GMScene.swift
+++ Metal006Buttons/Metal006Buttons/Shared/Game/SceneGraph/GMScene.swift
@@ -508,22 +508,3 @@ struct GMSceneImmutableNode: GMSceneNode {
         case backward
     }
 }
-
-extension float2 where Scalar == Float  {
-    func displayToNdc(display: float2) -> float2 {
-        let x = ((x / display.x) * 2) - 1
-        let y = 1 - ((y / display.y) * 2)
-        return float2(x, y)
-    }
-
-    func displayToNdc(display: float2) -> float4x4 {
-        let x = ((x / display.x) * 2) - 1
-        let y = 1 - ((y / display.y) * 2)
-        return float4x4(
-            [x, 0.0, 0.0, 0.0],
-            [0.0, y, 0.0, 0.0],
-            [0.0, 0.0, 1.0, 0.0],
-            [0.0, 0.0, 0.0, 1.0]
-        )
-    }
-}
