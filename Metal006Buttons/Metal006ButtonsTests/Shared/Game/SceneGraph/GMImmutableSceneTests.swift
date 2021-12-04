906b8d97c56c83df8bce717422bbdedc668936fa
David Kanenwisher <d.kanen@gmail.com>
2021-12-03 21:51:41 -0600

    WIP thinking about testing

--- Metal006Buttons/Metal006ButtonsTests/Shared/Game/SceneGraph/GMImmutableSceneTests.swift
+++ Metal006Buttons/Metal006ButtonsTests/Shared/Game/SceneGraph/GMImmutableSceneTests.swift
@@ -2,7 +2,43 @@
 // Created by David Kanenwisher on 12/3/21.
 //
 
-import Foundation
+import XCTest
+import simd
+@testable import Metal006Buttons
 
 class GMImmutableSceneTests {
+    func testClickMiddleButton() {
+        let scene = createScene()
+    }
+}
+
+extension GMImmutableSceneTests {
+    func createScene() -> RenderableCollection {
+        let root = GMImmutableNode()
+
+        var children: [GMImmutableNode] = []
+        (0..<10).forEach { _ in
+            children.append(
+                GMImmutableNode(
+                    children: [],
+                    location: Point(
+                        Float.random(in: -1...1),
+                        Float.random(in: -1...1),
+                        Float.random(in: 0...1)
+                    ),
+                    transformation: matrix_identity_float4x4,
+                    vertices: VerticeCollection().c[.cube]!,
+                    color: Float4(.green),
+                    state: .forward,
+                    hidden: false
+                )
+            )
+        }
+
+
+        return GMImmutableScene(
+            node: root.setChildren(children),
+            camera: GMImmutableCamera.atOrigin()
+        )
+    }
 }
