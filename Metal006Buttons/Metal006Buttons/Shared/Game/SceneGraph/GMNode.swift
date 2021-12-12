b838cf72cef2a3cb67aae68155b837735a1aa8c6
David Kanenwisher <d.kanen@gmail.com>
2021-12-12 16:16:50 -0600

    Add TODO with notes about GMNode

--- Metal006Buttons/Metal006Buttons/Shared/Game/SceneGraph/GMNode.swift
+++ Metal006Buttons/Metal006Buttons/Shared/Game/SceneGraph/GMNode.swift
@@ -11,8 +11,12 @@ protocol GMNode {
 
     func delete(child: GMNode) -> GMNode
 
-    //TODO remove everything that isn't related to managing the tree
-    //TODO RenderableNode
+    //TODO this should update by finding the old node and replace
+    // it with the new node. This update is a different sort of update.
+    // It has a knowledge of traversing the whole tree in order to change all
+    // nodes in the same way. This may be useful but maybe should be updateAll?
+    // Plus it assumes that all nodes have the same element. I might be able to
+    // get the same effect with traverse.
     func update(transform: (GMNode) -> GMNode) -> GMNode
 
     func setChildren(_ children: [GMNode]) -> GMNode
