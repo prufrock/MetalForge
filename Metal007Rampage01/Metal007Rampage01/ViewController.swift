ca17ef94e6042678882bcf25b9fcbbf2c6bfb8cf
David Kanenwisher <d.kanen@gmail.com>
2021-12-13 21:29:05 -0600

    Metal007Rampage01: Setup the MTKView

--- Metal007Rampage01/Metal007Rampage01/ViewController.swift
+++ Metal007Rampage01/Metal007Rampage01/ViewController.swift
@@ -6,14 +6,25 @@
 //
 
 import UIKit
+import MetalKit
 
 class ViewController: UIViewController {
+    private let metalView = MTKView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupMetalView()
     }
 
-
+    func setupMetalView() {
+        view.addSubview(metalView)
+        metalView.translatesAutoresizingMaskIntoConstraints = false
+        metalView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        metalView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        metalView.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+        metalView.heightAnchor.constraint(equalTo: view.heightAnchor).isActive = true
+        metalView.contentMode = .scaleAspectFit
+        metalView.backgroundColor = .black
+    }
 }
 
