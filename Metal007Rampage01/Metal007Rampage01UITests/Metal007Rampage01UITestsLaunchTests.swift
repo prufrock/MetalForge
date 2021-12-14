46aeb7637d36778364d2963b2f170971317d8a24
David Kanenwisher <d.kanen@gmail.com>
2021-12-13 21:19:23 -0600

    Create Metal007Rampage01
    Going to take a run at the RetroRampage tutorials but use Metal instead of UIImage.

---
+++ Metal007Rampage01/Metal007Rampage01UITests/Metal007Rampage01UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Metal007Rampage01UITestsLaunchTests.swift
+//  Metal007Rampage01UITests
+//
+//  Created by David Kanenwisher on 12/13/21.
+//
+
+import XCTest
+
+class Metal007Rampage01UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
