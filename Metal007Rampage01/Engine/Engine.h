b0d8d6b75e29cf82207c32e70e494b72eedc44fc
David Kanenwisher <d.kanen@gmail.com>
2021-12-13 21:25:40 -0600

    Metal007Rampage01: Create the Engine target

---
+++ Metal007Rampage01/Engine/Engine.h
@@ -0,0 +1,18 @@
+//
+//  Engine.h
+//  Engine
+//
+//  Created by David Kanenwisher on 12/13/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Engine.
+FOUNDATION_EXPORT double EngineVersionNumber;
+
+//! Project version string for Engine.
+FOUNDATION_EXPORT const unsigned char EngineVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Engine/PublicHeader.h>
+
+
