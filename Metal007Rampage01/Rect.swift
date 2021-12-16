3c36932683e2bb3c9c90c5fb1489ccc37be38f97
David Kanenwisher <d.kanen@gmail.com>
2021-12-15 21:42:47 -0600

    Metal007Rampage01: Make the upper right of the rect 0
    Like the Bitmap in the tutorial

--- Metal007Rampage01/Rect.swift
+++ Metal007Rampage01/Rect.swift
@@ -15,14 +15,12 @@ struct Rect {
     func shape() -> Shape {
         var shape = Shape(vertices: [])
         for y in min.y ..< max.y {
+            let v = max.y - y
             for x in min.x ..< max.x {
-                shape.vertices.append((Float4(Float(x), Float(y), 0.0, 1.0), .white))
+                shape.vertices.append((Float4(Float(x), Float(v), 0.0, 1.0), .white))
             }
         }
         shape.vertices[0] = (shape.vertices[0].0, .blue)
-        shape.vertices[2] = (shape.vertices[2].0, .blue)
-        shape.vertices[4] = (shape.vertices[4].0, .blue)
-        shape.vertices[6] = (shape.vertices[6].0, .blue)
         return shape
     }
 }
@@ -33,5 +31,9 @@ struct Vector {
 
 struct Shape {
     var vertices: [(Float4, Colors)]
+
+    func update(at:Int) {
+        let index = ()
+    }
 }
 
