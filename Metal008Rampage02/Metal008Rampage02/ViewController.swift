aae87a6893a77bf67ae6cc0409117c7ff5f13437
David Kanenwisher <d.kanen@gmail.com>
2021-12-26 14:07:32 -0600

    Metal007Rampage02: Start part 2

---
+++ Metal008Rampage02/Metal008Rampage02/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Metal008Rampage02
+//
+//  Created by David Kanenwisher on 12/26/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
