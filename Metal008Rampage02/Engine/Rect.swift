8755fb77dc9d14bb5297f4e12a386c6e6971ab43
David Kanenwisher <d.kanen@gmail.com>
2021-12-26 21:39:57 -0600

    Metal008Rampage02: Implement collision response
    so cool

--- Metal008Rampage02/Engine/Rect.swift
+++ Metal008Rampage02/Engine/Rect.swift
@@ -27,3 +27,25 @@ public struct Rect {
         return (vertices, Float4x4.init(scaleX: 1, y: -1, z: 1), .blue)
     }
 }
+
+public extension Rect {
+    func intersection(with rect: Rect) -> Float2? {
+        let left = Float2(x: max.x - rect.min.x, y: 0)
+        if left.x <= 0 {
+            return nil
+        }
+        let right = Float2(x: min.x - rect.max.x, y: 0)
+        if right.x >= 0 {
+            return nil
+        }
+        let up = Float2(x: 0, y: max.y - rect.min.y)
+        if up.y <= 0 {
+            return nil
+        }
+        let down = Float2(x: 0, y: min.y - rect.max.y)
+        if down.y >= 0 {
+            return nil
+        }
+        return [left, right, up, down].sorted(by: { $0.length < $1.length}).first
+    }
+}
