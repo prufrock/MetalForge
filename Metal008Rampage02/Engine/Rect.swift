6f130e74bcf9c2021ba1a698f5e171a99c66147b
David Kanenwisher <d.kanen@gmail.com>
2021-12-27 20:32:48 -0600

    Metal008Rampage02: Perform the adjustment with a linear transformation

--- Metal008Rampage02/Engine/Rect.swift
+++ Metal008Rampage02/Engine/Rect.swift
@@ -6,6 +6,7 @@ import simd
 
 public struct Rect {
     var min, max: Float2
+    private let adj: Float = -0.5
 
     public init(min: Float2, max: Float2) {
         self.min = min
@@ -15,14 +16,16 @@ public struct Rect {
     func renderable() -> ([Float4], Float4x4, Color) {
         var vertices:[Float4] = []
 
-        //TODO re-work rendering to not need this -0.5 adjustment
-        vertices.append(Float4(min.x - 0.5, max.y - 0.5, 0.0, 1.0))
-        vertices.append(Float4(max.x - 0.5, max.y - 0.5, 0.0, 1.0))
-        vertices.append(Float4(max.x - 0.5, min.y - 0.5, 0.0, 1.0))
+        vertices.append(Float4(min.x, max.y, 0.0, 1.0))
+        vertices.append(Float4(max.x, max.y, 0.0, 1.0))
+        vertices.append(Float4(max.x, min.y, 0.0, 1.0))
 
-        vertices.append(Float4(min.x - 0.5, max.y - 0.5, 0.0, 1.0))
-        vertices.append(Float4(min.x - 0.5, min.y - 0.5, 0.0, 1.0))
-        vertices.append(Float4(max.x - 0.5, min.y - 0.5, 0.0, 1.0))
+        vertices.append(Float4(min.x, max.y, 0.0, 1.0))
+        vertices.append(Float4(min.x, min.y, 0.0, 1.0))
+        vertices.append(Float4(max.x, min.y, 0.0, 1.0))
+
+        //TODO re-work rendering to not need this -0.5 adjustment
+        vertices = vertices.map{ Float4x4.init(translateX: adj, y: adj, z: 0.0) * $0 }
 
         return (vertices, Float4x4.init(scaleX: 1, y: -1, z: 1), .blue)
     }
