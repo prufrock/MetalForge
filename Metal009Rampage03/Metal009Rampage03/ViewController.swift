e4d4dc0158e6a4b98357e7b9da965eb87768eda8
David Kanenwisher <d.kanen@gmail.com>
2021-12-29 18:52:24 -0600

    Metal009Rampage03: Start on Rampage03

---
+++ Metal009Rampage03/Metal009Rampage03/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Metal009Rampage03
+//
+//  Created by David Kanenwisher on 12/29/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
