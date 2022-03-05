601519c31ee3276618a611cdd567bb14127e2f19
David Kanenwisher <d.kanen@gmail.com>
2022-03-05 17:22:24 -0600

    Start MetalRampageContd
    A place for the rest of the work on the metal renderer for the RetroRampage tutorials.

---
+++ MetalRampageContd/MetalRampageContd macOS/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  MetalRampageContd macOS
+//
+//  Created by David Kanenwisher on 3/5/22.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
