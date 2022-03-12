758e3a581cedb5b185a1e28eec22659b93a70c7d
David Kanenwisher <d.kanen@gmail.com>
2022-03-11 21:22:13 -0600

    Change the rotate functions used in iOS and tvOS GameViewControllers

--- MetalRampageContd/MetalRampageContd iOS/GameViewController.swift
+++ MetalRampageContd/MetalRampageContd iOS/GameViewController.swift
@@ -80,7 +80,7 @@ extension GameViewController: MTKViewDelegate {
         let input = Input(
             speed: -inputVector.y,
             rotation: Float2x2(rotate: rotation),
-            rotation3d: Float4x4(rotateY: inputVector.x * world.player.turningSpeed * worldTimeStep),
+            rotation3d: Float4x4.rotateY(inputVector.x * world.player.turningSpeed * worldTimeStep),
             showMap: false,
             drawWorld: true
         )
