e228362f7e2b9e8d238671c3f04cf1614776fdba
David Kanenwisher <d.kanen@gmail.com>
2022-05-07 15:35:01 -0500

    Rampage17: add Font

---
+++ MetalRampageContd/MetalRampageContd Shared/Font.swift
@@ -0,0 +1,11 @@
+//
+//  Font.swift
+//  MetalRampageContd
+//
+//  Created by David Kanenwisher on 5/7/22.
+//
+
+struct Font: Decodable {
+    let texture: Texture
+    let characters: [String]
+}
