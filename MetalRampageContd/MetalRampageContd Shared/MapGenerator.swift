4189dc3e20bc04b789e72793a759580ec07a4191
David Kanenwisher <d.kanen@gmail.com>
2022-05-11 21:24:26 -0500

    Rampage18:  move adding things to an extension function

--- MetalRampageContd/MetalRampageContd Shared/MapGenerator.swift
+++ MetalRampageContd/MetalRampageContd Shared/MapGenerator.swift
@@ -7,16 +7,16 @@
  */
 struct MapGenerator {
     private(set) var map: Tilemap
+    // keep track of the players starting position
+    // this makes it so we can adjust the level around them
+    private var playerPosition: Float2!
+    private var emptyTiles: [Float2] = []
 
     public init(mapData: MapData, index: Int) {
         map = Tilemap(mapData, index: index)
 
         // Find empty tiles
         // Find them in a systematic way so we don't have to sit around all day waiting
-        var emptyTiles = Set<Float2>()
-        // keep track of the players starting position
-        // this makes it so we can adjust the level around them
-        var playerPosition: Float2!
         for y in 0 ..< map.height {
             for x in 0 ..< map.width {
                 // the 0.5 offsets are so it's in the middle of the tile.
@@ -26,7 +26,7 @@ struct MapGenerator {
                     switch map[thing: x, y] {
                     case .nothing:
                         // it's nothing! put it in the bag!
-                        emptyTiles.insert(position)
+                        emptyTiles.append(position)
                     case .player:
                         // it's the player so keep track of that!
                         playerPosition = position
@@ -39,35 +39,34 @@ struct MapGenerator {
 
         // Add monsters
         for _ in 0 ..< (mapData.monsters ?? 0) {
-            if let position = emptyTiles.filter({
-                    // give the player some breathing room
-                    // 2.5 units of breathing room
-                    (playerPosition - $0).length > 2.5
-                }).randomElement() {
-                // The position
-                let x = Int(position.x), y = Int(position.y)
-                // this position is no a monster!
-                map[thing: x, y] = .monster
-                // Don't let the tile be used again
-                emptyTiles.remove(position)
-            }
+            add(.monster, at: emptyTiles.filter({
+                        // give the player some breathing room
+                        // 2.5 units of breathing room
+                        (playerPosition - $0).length > 2.5
+                    }).randomElement())
         }
 
         // Add healing potions
         for _ in 0 ..< (mapData.healingPotions ?? 0) {
-            if let position = emptyTiles.randomElement() {
-                let x = Int(position.x), y = Int(position.y)
-                map[thing: x, y] = .healingPotion
-                emptyTiles.remove(position)
-            }
+            add(.healingPotion, at: emptyTiles.randomElement())
         }
 
         // Add fire blasts
         for _ in 0 ..< (mapData.fireBlasts ?? 0) {
-            if let position = emptyTiles.randomElement() {
-                let x = Int(position.x), y = Int(position.y)
-                map[thing: x, y] = .fireBlast
-                emptyTiles.remove(position)
+            add(.fireBlast, at: emptyTiles.randomElement())
+        }
+    }
+}
+
+private extension MapGenerator {
+    mutating func add(_ thing: Thing, at position: Float2?) {
+        if let position = position {
+            // put the thing there
+            map[thing: Int(position.x), Int(position.y)] = thing
+            // remove the tile that was used
+            // this way a different thing isn't placed there
+            if let index = emptyTiles.lastIndex(of: position) {
+                emptyTiles.remove(at: index)
             }
         }
     }
