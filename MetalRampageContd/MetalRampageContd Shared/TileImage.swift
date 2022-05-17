20d4ff65db154e679fc0b2a6ac927b9ff904c096
David Kanenwisher <d.kanen@gmail.com>
2022-05-16 21:32:16 -0500

    Remove rotate methods from TileImage

--- MetalRampageContd/MetalRampageContd Shared/TileImage.swift
+++ MetalRampageContd/MetalRampageContd Shared/TileImage.swift
@@ -45,14 +45,14 @@ struct TileImage {
                     let wallTiles = world.wallTiles(at: x, y)
                     myTiles.append((tile1, texCoords, Float4x4.translate(x: Float(x), y: Float(y), z: 0), wallColor, .triangle, map[x, y], Int2(x, y))) // bottom
 //                    myTiles.append((tile1, texCoords, Float4x4.translate(x: Float(x), y: Float(y), z: 1.0) * rotateX(.pi), .white, .triangle, map[x, y], Int2(x, y))) //top, off because it overlaps with the ceiling tiles on some tiles
-                    myTiles.append((tile1, texCoords, Float4x4.translate(x: Float(x) + 1.0, y: Float(y), z: 1.0) * rotateY(.pi/2), .black, .triangle, wallTiles.east, Int2(x, y)))
-                    myTiles.append((tile1, texCoords, Float4x4.translate(x: Float(x) + 1.0, y: Float(y) + 1.0, z: 1.0) * rotateZ(.pi/2) * rotateY(.pi/2), .black, .triangle, wallTiles.north, Int2(x, y)))
-                    myTiles.append((tile1, texCoords, Float4x4.translate(x: Float(x), y: Float(y), z: 1.0) * rotateZ((3 * .pi)/2) * rotateY(.pi/2), .black, .triangle, wallTiles.south, Int2(x, y)))
-                    myTiles.append((tile1, texCoords, Float4x4.translate(x: Float(x), y: Float(y) + 1.0, z: 1.0) * rotateZ((2 * .pi)/2) * rotateY(.pi/2), .black, .triangle, wallTiles.west, Int2(x, y)))
+                    myTiles.append((tile1, texCoords, Float4x4.translate(x: Float(x) + 1.0, y: Float(y), z: 1.0) * Float4x4.rotateY(.pi/2), .black, .triangle, wallTiles.east, Int2(x, y)))
+                    myTiles.append((tile1, texCoords, Float4x4.translate(x: Float(x) + 1.0, y: Float(y) + 1.0, z: 1.0) * Float4x4.rotateZ(.pi/2) * Float4x4.rotateY(.pi/2), .black, .triangle, wallTiles.north, Int2(x, y)))
+                    myTiles.append((tile1, texCoords, Float4x4.translate(x: Float(x), y: Float(y), z: 1.0) * Float4x4.rotateZ((3 * .pi)/2) * Float4x4.rotateY(.pi/2), .black, .triangle, wallTiles.south, Int2(x, y)))
+                    myTiles.append((tile1, texCoords, Float4x4.translate(x: Float(x), y: Float(y) + 1.0, z: 1.0) * Float4x4.rotateZ((2 * .pi)/2) * Float4x4.rotateY(.pi/2), .black, .triangle, wallTiles.west, Int2(x, y)))
                 }
                 if !map[x, y].isWall {
                     myTiles.append((tile1, texCoords, Float4x4.translate(x: Float(x), y: Float(y), z: 0), wallColor, .triangle, map[x, y], Int2(x, y)))
-                    myTiles.append((tile1, texCoords, Float4x4.translate(x: Float(x) + 1.0, y: Float(y), z: 1.0)  * rotateY(.pi), wallColor, .triangle, .ceiling, Int2(x, y)))
+                    myTiles.append((tile1, texCoords, Float4x4.translate(x: Float(x) + 1.0, y: Float(y), z: 1.0)  * Float4x4.rotateY(.pi), wallColor, .triangle, .ceiling, Int2(x, y)))
                 }
             }
         }
@@ -60,21 +60,5 @@ struct TileImage {
         tiles = myTiles.map { vertices, uv, transform, color, primitiveType, tile, position in
             (RNDRObject(vertices: vertices, uv: uv, transform: transform, color: color, primitiveType: primitiveType, position: position), tile)
         }
-    }
-
-    //TODO find a home for these
-    private func rotateY(_ angle: Float) -> Float4x4 {
-        Float4x4.identity()
-            * Float4x4.rotateY(angle)
-    }
-
-    private func rotateZ(_ angle: Float) -> Float4x4 {
-        Float4x4.identity()
-            * Float4x4.rotateZ(angle)
-    }
-
-    private func rotateX(_ angle: Float) -> Float4x4 {
-        Float4x4.identity()
-            * Float4x4.rotateX(angle)
     }
 }
